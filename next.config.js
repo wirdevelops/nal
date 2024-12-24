7e8e3ca775a292ce6732bda088145edb646c63ac
wirdevelops <wirdeveloper@gmail.com>
2024-12-24 08:28:58 +0100

    Updated deployment configurations

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
+  images: {
+    domains: ['fonts.gstatic.com'],
+    unoptimized: true
+  },
+  experimental: {
+    appDir: true
+  }
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
