c96f732eaa399bd1059e874dc48e69d71a10accb
wirdevelops <wirdeveloper@gmail.com>
2025-02-01 13:09:15 +0100

    Many many more more updated for production deployment

--- src/components/ui/phone-input.tsx
+++ src/components/ui/phone-input.tsx
@@ -56,3 +56,50 @@ export const PhoneInput = forwardRef<HTMLInputElement, InputProps>(
 );
 
 PhoneInput.displayName = 'PhoneInput';
+
+
+// 'use client';
+
+// import { forwardRef } from 'react';
+// import { Input, InputProps } from '@/components/ui/input';
+// import { AsYouType } from 'libphonenumber-js';
+
+// export const PhoneInput = forwardRef<HTMLInputElement, InputProps>(
+//   ({ value, onChange, ...props }, ref) => {
+//     const formatPhoneNumber = (value: string) => {
+//       try {
+//         const formatter = new AsYouType();
+//         return formatter.input(value);
+//       } catch (error) {
+//         return value;
+//       }
+//     };
+
+//     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//       const cleanValue = e.target.value.replace(/[^\d+]/g, '');
+//       const formatted = formatPhoneNumber(cleanValue);
+      
+//       if (onChange) {
+//         onChange({
+//           ...e,
+//           target: {
+//             ...e.target,
+//             value: formatted
+//           }
+//         });
+//       }
+//     };
+
+//     return (
+//       <Input
+//         ref={ref}
+//         value={value}
+//         onChange={handleChange}
+//         placeholder="+237 6 79 04 73 69"
+//         {...props}
+//       />
+//     );
+//   }
+// );
+
+// PhoneInput.displayName = 'PhoneInput';
